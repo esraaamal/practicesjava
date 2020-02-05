cf02a58a4acc04b7d3b91595004dd87b3fbf8f6f
esraaamal <ma.esraa@hotmail.com>
2020-02-05 20:52:35 +0200

    ffrd

--- starter-code/javascript/add-contact.js
+++ starter-code/javascript/add-contact.js
@@ -1,16 +1,18 @@
-var today =new Date();
-var hourNow =today.getHours();
+function greatGreating(){
+var hourNow;
+hourNow=prompt('what is time now? 0-24');
+var name ;
+name= prompt('what is your name');
 var greeting;
-if (hourNow > 18) {
-    greeting='good evennig!';
-    
-} else if(hourNow >12){
-    greeting='good evennig!';
-} else if(hourNow >0){
-    greeting='good morning!';
+if (hourNow>0 && hourNow<=12)
+{greeting='Good morning!';}
+else if (hourNow>12 && hourNow<=18)
+{greeting='Good afternon!';}
+else if (hourNow>18 && hourNow<=24)
+{greeting='Good night';}
+else{greeting = 'welcome';
 }
-else {
-    greeting='welcome!';
 
+document.write('<h3>' + greeting + '  ' + name + '</h3>' )
 }
-document.write('<h3>' + greeting + '</h3>');
+greatGreating();
