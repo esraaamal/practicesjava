874bc77f3632a66e7e57de5626de9a2da7195cb2
esraaamal <ma.esraa@hotmail.com>
2020-02-05 21:55:54 +0200

    ffrd

--- starter-code/javascript/add-contact.js
+++ starter-code/javascript/add-contact.js
@@ -1,18 +1,32 @@
-function greatGreating(){
-var hourNow;
-hourNow=prompt('what is time now? 0-24');
-var name ;
-name= prompt('what is your name');
-var greeting;
-if (hourNow>0 && hourNow<=12)
-{greeting='Good morning!';}
-else if (hourNow>12 && hourNow<=18)
-{greeting='Good afternon!';}
-else if (hourNow>18 && hourNow<=24)
-{greeting='Good night';}
-else{greeting = 'welcome';
-}
 
-document.write('<h3>' + greeting + '  ' + name + '</h3>' )
+
+
+
+
+
+
+var showOrder =function (){
+var userOrder;
+var numberOfImage;
+var orderImage = '';
+while(userOrder !=="house" && userOrder !== "hotel"){
+    userOrder=prompt("please enter house or hotel !") ;
+    numberOfImage=prompt("how many image do you want ?");
+    for(var i=0 ; i<numberOfImage ;i++)
+    {console.log("index is"+i);
+        if (userOrder ==="house")
+{orderImage=orderImage +'<img src="images/house.png">'   ;}
+else if (orderImage ==="hotle")
+{orderImage=orderImage +'<img src="images/hotle.png">';}
+console.log(orderImage);
+    }return orderImage;
 }
-greatGreating();
+}
+document.write('<p>'+showOrder()+ '</p>' )
+
+
+
+
+
+
+
